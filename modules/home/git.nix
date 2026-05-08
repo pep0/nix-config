6278027ececce30a59ea16df26b40602f8d5f417
pep0 <baschy@msn.com>
2026-05-08 13:21:34 +0200

    feat: sign commits with ssh key

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -10,6 +10,16 @@
       options.navigate = true;
     };
 
+    # Sign commits with SSH using the same key that authenticates
+    # pushes. Add the public key to GitHub under
+    # https://github.com/settings/ssh/new with type "Signing Key" to
+    # make the "Verified" badge appear on commits.
+    signing = {
+      signByDefault = true;
+      format = "ssh";
+      key = "~/.ssh/id_ed25519.pub";
+    };
+
     extraConfig = {
       init.defaultBranch = "main";
       pull.rebase = true;
